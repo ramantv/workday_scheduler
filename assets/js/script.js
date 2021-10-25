aa934706dd028d4443b3ca110aa1111be6d32c2a
ramantv <90482664+ramantv@users.noreply.github.com>
2021-10-24 23:52:22 -0500

    v0.9 - Planner display details created.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var workDayDetails = [];
+var workDaySchedule = [];
 const workdayStartHour = 9;
 const workdayEndHour = 17; // 9am - 5pm
 
@@ -33,19 +33,22 @@ function setTodaysDate() {
   $("#currentDay").text(today);
 }
 
+//saves data to localStorage
+function savePlannerData() {
+  localStorage.setItem("workDaySchedule", JSON.stringify(workDaySchedule));
+}
+
 // Initialized the workDay with an empty array of hourPlanner objects
 function initializePlannerData() {
-
-    var arrIndex = 0;
-    var hrDetail = "";
-    var dispHr = "";
-    var ampm = "";
+  var arrIndex = 0;
+  var hrDetail = "";
+  var dispHr = "";
+  var ampm = "";
 
   for (let hr = workdayStartHour; hr <= workdayEndHour; hr++) {
-    //get index and set variable to store data in array
-    arrIndex = hr - 9;
-    dispHr = getDisplayHour(hr);
-    ampm = getAMorPMforHour(hr);
+    arrIndex = hr - 9; // to save the index of the hour. 0 is 9am, 1 is 10am etc
+    dispHr = getDisplayHour(hr); // the actual hour. 9, 10, 11, 12, 1...5
+    ampm = getAMorPMforHour(hr); // the am or pm tag based on the 24 hour time.
 
     hourPlanner = {
       id: arrIndex,
@@ -55,12 +58,52 @@ function initializePlannerData() {
       hourDetails: hrDetail,
     };
 
-    workDayDetails.push(hourPlanner);
+    workDaySchedule.push(hourPlanner);
   }
+}
+
+// creates the display emalements for the workday schedule
+function createTimeBlocksforDisplay() {
+
+  workDaySchedule.forEach(function (hrPlan) {
+    // creates row
+    var timeBlock = $("<form>").addClass("row");
+
+    $(".container").append(timeBlock);
+
+    //creates field for time
+    var timeField = $("<div>")
+      .addClass("col-md-2 hour")
+      .text(hrPlan.displayHour + hrPlan.ampm);
+
+    // creates the input data fiels 
+    var hourInput = $("<div>").addClass("col-md-9 description p-0");
+    var hourData = $("<textarea>");
+    hourData.attr("id", hrPlan.id);
+
+    //compare time to current time - color codes
+    if (hrPlan.time == moment().format("HH")) {
+      hourData.addClass("present");
+    } else if (hrPlan.time < moment().format("HH")) {
+      hourData.addClass("past");
+    } else if (hrPlan.time > moment().format("HH")) {
+      hourData.addClass("future");
+    }
+
+    hourInput.append(hourData);
+
+    // create save button for end of row
+    var saveIcon = $("<i class='far fa-save fa-lg'></i>");
+    var saveEnd = $("<button>").addClass("col-md-1 saveBtn");
+
+    //append elements to row
+    saveEnd.append(saveIcon);
+    timeBlock.append(timeField, hourInput, saveEnd);
+  });
 }
 
 $(document).ready(function () {
   initializePlannerData();
-
   setTodaysDate();
+  createTimeBlocksforDisplay();
 });
